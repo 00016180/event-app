2c459a4cd260a6a3af2126270494e38258a33ed4
00016180 <00016180wiut@gmail.com>
2024-03-10 20:19:06 +0500

    added web controller, views and routes, public files

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const path = require('path');
-
+const web_route = require('./routes/web');
 const app = express();
 
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
-app.get('/', (req, res)=>{
-    res.send('Hello world!')
-});
+app.set('view engine', 'pug');
 
+app.use('/css', express.static('public/css'))
+app.use('/js', express.static('public/js'))
+
+app.use('/', web_route);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log(`Listening in port ${port}`)});
---
+++ routes/web/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const home_router = require('./home')
+const router = express.Router();
+
+router.use('/', home_router);
+module.exports = router;
